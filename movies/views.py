52c515672706487ff8239c857317435a9a41b076
Noah Amoo <yawnoah3n@yahoo.com>
2025-09-06 14:48:49 +0100

    Defined the logic to add new movie in views

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Movie
 
@@ -14,4 +14,11 @@ def detail(request, id):
     return render(request, 'movies/detail.html', {'movie': data})
 
 def add(request):
+    title = request.POST.get('title') 
+    year = request.POST.get('year')
+
+    if title and year:
+        movie = Movie(title=title, year=year)
+        movie.save()
+        return HttpResponseRedirect('/movies')
     return render(request, 'movies/add.html')
